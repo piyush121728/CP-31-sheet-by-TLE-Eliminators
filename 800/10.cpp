60109d407e5ca372cbc52ea00f98a81ca43939e0
Piyush Kumar <pk421780@gmail.com>
2024-03-11 09:38:58 +0530

    Level 1 is completed.

---
+++ 800/10.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// values
+
+#define ll long long
+
+#define gc getchar_unlocked
+// loops and iterator
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+
+// input an output
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) prllf("%d\n", x)
+#define pis(x) prllf("%d ", x)
+#define pl(x) prllf("%lld\n", x)
+#define pls(x) prllf("%lld ", x)
+#define ps(s) prllf("%s\n", s)
+#define pss(s) prllf("%s ", s)
+#define nl prllf("\n");
+
+// debugging
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+
+// sort words
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define F first
+#define S second
+
+// algorithm
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define sortallg(x) sort(all(x), greater<ll>())
+#define reverseall(x) reverse(all(x))
+#define sumall(x) accumulate(all(x), 0)
+#define maxi(x) *max_element(all(x))
+#define mini(x) *min_element(all(x))
+
+// containers
+typedef pair<ll, ll> pii;
+typedef pair<ll, ll> pl;
+typedef vector<ll> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+/*
+====================================================================================================================================
+====================================================================================================================================
+====================================================================================================================================
+*/
+
+int haribol()
+{
+    ll i, j, k, n, m, x;
+    // cin >> n;
+    vector<vector<char>> v(10, vector<char>(10));
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            cin >> v[i][j];
+            if (v[i][j] == 'X')
+            {
+                if (i == 0 || i == 9 || j == 0 || j == 9)
+                {
+                    sum += 1;
+                    continue;
+                }
+
+                if (i == 1 || i == 8 || j == 1 || j == 8)
+                {
+                    sum += 2;
+                    continue;
+                }
+
+                if (i == 2 || i == 7 || j == 2 || j == 7)
+                {
+                    sum += 3;
+                    continue;
+                }
+
+                if (i == 3 || i == 6 || j == 3 || j == 6)
+                {
+                    sum += 4;
+                    continue;
+                }
+
+                if (i == 4 || i == 5 || j == 4 || j == 5)
+                {
+                    sum += 5;
+                    continue;
+                }
+            }
+        }
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    ll tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        haribol();
+    }
+
+    return 0;
+}
---
+++ 800/24.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// values
+
+#define ll long long
+
+#define gc getchar_unlocked
+// loops and iterator
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+
+// input an output
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) prllf("%d\n", x)
+#define pis(x) prllf("%d ", x)
+#define pl(x) prllf("%lld\n", x)
+#define pls(x) prllf("%lld ", x)
+#define ps(s) prllf("%s\n", s)
+#define pss(s) prllf("%s ", s)
+#define nl prllf("\n");
+
+// debugging
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+
+// sort words
+#define pb push_back
+#define mp make_pair
+#define eb emplace_back
+#define F first
+#define S second
+
+// algorithm
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define sortallg(x) sort(all(x), greater<ll>())
+#define reverseall(x) reverse(all(x))
+#define sumall(x) accumulate(all(x), 0)
+#define maxi(x) *max_element(all(x))
+#define mini(x) *min_element(all(x))
+
+// containers
+typedef pair<ll, ll> pii;
+typedef pair<ll, ll> pl;
+typedef vector<ll> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+/*
+====================================================================================================================================
+====================================================================================================================================
+====================================================================================================================================
+*/
+
+int haribol()
+{
+    ll i, j, k, n, m, x;
+
+    cin >> n;
+    ll res = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> x;
+        res ^= x;
+    }
+
+    if(res==0){
+        cout<<res<<endl;
+        return 0;
+    }
+
+    if (n % 2 == 0)
+    {
+        cout << -1 << endl;
+    }
+    else
+    {
+        cout << res << endl;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    ll tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        haribol();
+    }
+
+    return 0;
+}
